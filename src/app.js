61e9f7bf7bb8f9de9710292f5e5bf01978070854
j4mieLeigh <149057654+j4mieLeigh@users.noreply.github.com>
2023-11-16 10:13:26 +0100

    Updated js function to display forecast starting next day, adjusted positioning in css

--- src/app.js
+++ src/app.js
@@ -91,7 +91,7 @@ function displayForecast(response) {
   let forecastHtml = "";
 
   response.data.daily.forEach(function (day, index) {
-    if (index < 5) {
+    if (index > 0 && index < 6) {
       forecastHtml =
         forecastHtml +
         `
